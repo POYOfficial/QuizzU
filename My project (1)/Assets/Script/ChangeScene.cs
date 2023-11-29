97df8b5f096feb729fea8fddc4e2c73f04783318
Kyllian Favre <122874779+Garax340@users.noreply.github.com>
2023-11-29 10:14:26 +0100

    ChangeSceneScript

--- My project (1)/Assets/Script/ChangeScene.cs
+++ My project (1)/Assets/Script/ChangeScene.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int up;
+    public void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            SceneManager.LoadScene(up);
+        }
     }
 }
