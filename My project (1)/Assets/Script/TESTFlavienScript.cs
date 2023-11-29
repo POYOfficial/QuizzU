481a1b883b97d46627f0088b0ef1b1f67619ad5d
S-n-o-w-m-y <122878714+S-n-o-w-m-y@users.noreply.github.com>
2023-11-29 09:39:18 +0100

    fe

--- My project (1)/Assets/Script/TESTFlavienScript.cs
+++ My project (1)/Assets/Script/TESTFlavienScript.cs
@@ -6,33 +6,33 @@ public class TESTFlavienScript : MonoBehaviour
 {
 
     public GameObject equipe1;
-    public float vote1;
+    private float vote1;
     public GameObject equipe2;
-    public float vote2;
+    private float vote2;
     public GameObject equipe3;
-    public float vote3;
+    private float vote3;
     public GameObject equipe4;
-    public float vote4;
+    private float vote4;
     public GameObject equipe5;
-    public float vote5;
+    private float vote5;
     public GameObject equipe6;
-    public float vote6;
+    private float vote6;
     public GameObject equipe7;
-    public float vote7;
+    private float vote7;
     public GameObject equipe8;
-    public float vote8;
+    private float vote8;
     public GameObject equipe9;
-    public float vote9;
+    private float vote9;
     public GameObject equipe10;
-    public float vote10;
+    private float vote10;
     public GameObject equipe11;
-    public float vote11;
+    private float vote11;
     public GameObject equipe12;
-    public float vote12;
+    private float vote12;
     public GameObject equipe13;
-    public float vote13;
+    private float vote13;
     public GameObject equipe14;
-    public float vote14;
+    private float vote14;
     private bool stopTest;
     public float Maxvote;
     void Update()
@@ -46,13 +46,113 @@ public class TESTFlavienScript : MonoBehaviour
             vote1 = 1;
 
         }
-        if (Input.GetKey(KeyCode.Z) && !stopTest)
+        if (Input.GetKey(KeyCode.Z) && !stopTest && vote2<Maxvote)
         {
             stopTest = true;
             equipe2.SetActive(true);
             StartCoroutine(Cooldown());
+            vote2 = 1;
 
         }
+        if (Input.GetKey(KeyCode.E) && !stopTest && vote3 < Maxvote)
+        {
+            stopTest = true;
+            equipe3.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote3 = 1;
+
+        }
+        if (Input.GetKey(KeyCode.R) && !stopTest && vote4 < Maxvote)
+        {
+            stopTest = true;
+            equipe4.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote4 = 1;
+
+        }
+        if (Input.GetKey(KeyCode.T) && !stopTest && vote5 < Maxvote)
+        {
+            stopTest = true;
+            equipe5.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote5 = 1;
+
+        }
+        if (Input.GetKey(KeyCode.Y) && !stopTest && vote6 < Maxvote)
+        {
+            stopTest = true;
+            equipe6.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote6 = 1;
+
+
+        }
+        if (Input.GetKey(KeyCode.U) && !stopTest && vote7 < Maxvote)
+        {
+            stopTest = true;
+            equipe7.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote7 = 1;
+
+
+        }
+        if (Input.GetKey(KeyCode.I) && !stopTest && vote8 < Maxvote)
+        {
+            stopTest = true;
+            equipe8.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote8 = 1;  
+
+        }
+        if (Input.GetKey(KeyCode.O) && !stopTest && vote9 < Maxvote)
+        {
+            stopTest = true;
+            equipe9.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote9 = 1;
+
+        }
+        if (Input.GetKey(KeyCode.P) && !stopTest && vote10 < Maxvote)
+        {
+            stopTest = true;
+            equipe10.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote10 = 1; 
+
+        }
+        if (Input.GetKey(KeyCode.Q) && !stopTest && vote11 < Maxvote)
+        {
+            stopTest = true;
+            equipe11.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote11 = 1;
+
+        }
+        if (Input.GetKey(KeyCode.S) && !stopTest && vote12 < Maxvote)
+        {
+            stopTest = true;
+            equipe12.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote12 = 1; 
+
+        }
+        if (Input.GetKey(KeyCode.D) && !stopTest && vote13 < Maxvote)
+        {
+            stopTest = true;
+            equipe13.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote13 = 1;
+
+        }
+        if (Input.GetKey(KeyCode.F) && !stopTest && vote14 < Maxvote)
+        {
+            stopTest = true;
+            equipe14.SetActive(true);
+            StartCoroutine(Cooldown());
+            vote14 = 1;
+
+        }
+
 
     }
 
@@ -62,6 +162,18 @@ public class TESTFlavienScript : MonoBehaviour
         stopTest = false;
         equipe1.SetActive(false);
         equipe2.SetActive(false);
+        equipe3.SetActive(false);
+        equipe4.SetActive(false);
+        equipe5.SetActive(false);
+        equipe6.SetActive(false);
+        equipe7.SetActive(false);
+        equipe8.SetActive(false);
+        equipe9.SetActive(false);
+        equipe10.SetActive(false);
+        equipe11.SetActive(false);
+        equipe12.SetActive(false);
+        equipe13.SetActive(false);
+        equipe14.SetActive(false);
     }
 
 }
